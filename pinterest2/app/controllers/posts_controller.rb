09c442e9a59210cf2cac299ebff421274d7eba4e
josedavidb <josedavidb6@gmail.com>
2017-11-28 19:48:56 -0400

    Function create, edit, delete a Post and show list of Post in the index

--- pinterest2/app/controllers/posts_controller.rb
+++ pinterest2/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :set_post, except:[:new,:create,:index]
 
 	def index
 	end
@@ -7,5 +8,73 @@ class PostsController < ApplicationController
 
 
 
-	
+	def index
+
+		@array_of_posts = Post.all
+
+
+	end
+
+	def show 
+
+	end
+
+	def new
+
+		@new_post = Post.new
+	end
+
+
+	def create
+
+
+
+		@new_post = Post.new(post_params)
+
+
+
+		if @new_post.save
+			redirect_to @new_post
+
+		else
+			render :new
+		end
+
+
+		
+	end
+
+	def delete
+		@array_of_posts = post.all
+	end
+
+	def destroy
+		@new_post.destroy
+		redirect_to posts_path
+	end
+
+	def edit
+		render :edit
+	end
+
+	def update
+		if @new_post.update(post_params)
+			redirect_to @new_post
+		else
+			render :edit
+		end
+	end
+
+	#STRONG_PARAM
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
+
+
+	def set_post
+		@new_post = Post.find(params[:id])
+end
 end
